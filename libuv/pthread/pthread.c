fac6aa0c8beb839afd2ebb4c43ccbde5fdd8944a
ben fleis <bfleis@linkedin.com>
2013-03-06 10:49:59 +0100

    add pthread example to libuv fix Makefiles to rm -rf on clean

--- libuv/pthread/pthread.c
+++ libuv/pthread/pthread.c
@@ -11,30 +11,31 @@
 
 #include <pthread.h>
 
-#include "ev.h"
+#include "uv.h"
 
-pthread_mutex_t lock;
-double timeout = 0.00001;
-ev_timer timeout_watcher;
-int timeout_count = 0;
+static pthread_mutex_t lock;
+static uint64_t timeout;
+static uv_timer_t timeout_watcher;
+static int timeout_count = 0;
 
-ev_async async_watcher;
-int async_count = 0;
+static uv_async_t async_watcher;
+static int async_count = 0;
 
-struct ev_loop* loop_for_async_thread;
+static uv_loop_t* loop;
+static uv_loop_t* loop_for_async_thread;
 
 void*
 run_other_thread(void* args)
 {
-    printf("Running secondary: ev_loop(loop_for_async_thread, 0);\n");
-    ev_loop(loop_for_async_thread, 0);
+    printf("Running secondary: uv_run(loop_for_async_thread, 0);\n");
+    uv_run(loop_for_async_thread, 0);
     return NULL;
 }
 
 static void
-async_cb(EV_P_ ev_async *w, int revents)
+async_cb(uv_async_t* w, int revents)
 {
-    //puts("async ready");
+    //fprintf(stderr, "async ready\n");
     pthread_mutex_lock(&lock);     // Don't forget locking
     ++async_count;
     printf("async = %d, timeout = %d\n", async_count, timeout_count);
@@ -42,59 +43,52 @@ async_cb(EV_P_ ev_async *w, int revents)
 }
 
 static void
-timeout_cb(EV_P_ ev_timer *w, int revents) // Timer callback function
+timeout_cb(uv_timer_t* w, int revents) // Timer callback function
 {
-    //puts("timeout");
-    if (!ev_async_pending(&async_watcher)) {
-        // the event has not yet been processed (or even noted) by the event
-        // loop? (i.e. Is it serviced? If yes then proceed to)
-        // Sends/signals/activates the given ev_async watcher, that is, feeds
-        // an EV_ASYNC event on the watcher into the event loop.
-        ev_async_send(loop_for_async_thread, &async_watcher);
-    }
+    //fprintf(stderr, "timeout\n");
+    uv_async_send(&async_watcher);
 
     pthread_mutex_lock(&lock);     // Don't forget locking
     ++timeout_count;
     pthread_mutex_unlock(&lock);   // Don't forget unlocking
     w->repeat = timeout;
-    ev_timer_again(loop, &timeout_watcher); // Start the timer again.
+    uv_timer_again(&timeout_watcher); // Start the timer again.
 }
 
 int
 main(int argc, const char* const* argv)
 {
     if (argc < 2) {
-        fprintf(stderr, "Timeout value missing.\n%s <timeout>", argv[0]);
+        fprintf(stderr, "Timeout value missing.\n%s <timeout>\n", argv[0]);
         return -1;
     }
-    timeout = atof(argv[1]);
-
-    struct ev_loop* loop = EV_DEFAULT;  // or ev_default_loop(0);
+    timeout = atoi(argv[1]);
 
     // Initialize pthread
     pthread_mutex_init(&lock, NULL);
     pthread_t thread;
 
-    // This loop sits in the pthread
-    loop_for_async_thread = ev_loop_new(0);
+    // loop runs under main thread, loop_for_async_thread runs in the pthread
+    loop = uv_default_loop();
+    loop_for_async_thread = uv_loop_new();
 
     // This block is specifically used pre-empting thread (i.e. temporary
     // interruption and suspension of a task, without asking for its
     // cooperation, with the intention to resume that task later.)
     //
     // This takes into account thread safety
-    ev_async_init(&async_watcher, async_cb);
-    ev_async_start(loop_for_async_thread, &async_watcher);
+    uv_async_init(loop_for_async_thread, &async_watcher, async_cb);
+    //uv_async_start(loop_for_async_thread, &async_watcher);
     pthread_create(&thread, NULL, run_other_thread, NULL);
 
     // Non repeating timer. The timer starts repeating in the timeout callback
     // function
-    ev_timer_init(&timeout_watcher, timeout_cb, timeout, 0.);
-    ev_timer_start(loop, &timeout_watcher);
+    uv_timer_init(loop, &timeout_watcher);
+    uv_timer_start(&timeout_watcher, timeout_cb, timeout, 0);
 
-    // now wait for events to arrive
-    printf("Running primary: ev_loop(loop, 0);\n");
-    ev_loop(loop, 0);
+    // now wait for uvents to arrive
+    printf("Running primary: uv_run(loop, 0);\n");
+    uv_run(loop, 0);
     // Wait on threads for execution
     pthread_join(thread, NULL);
 
